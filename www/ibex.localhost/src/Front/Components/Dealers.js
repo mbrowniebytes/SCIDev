9b15609cb8906ab56b768e95963ecd2fd7709fec
Michael Brown <mbrowniebytes@gmail.com>
2019-11-29 17:57:50 -0600

    add dealers

--- www/ibex.localhost/src/Front/Components/Dealers.js
+++ www/ibex.localhost/src/Front/Components/Dealers.js
@@ -1,17 +1,58 @@
 import React, {Component, Fragment} from "react";
-import ibexApp from 'ibexApp';
+
+import ReactTable from "react-table";
+import "react-table/react-table.css";
+import "./Dealers.css";
 
 export default class Dealers extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            dealers: [],
+        };
+    }
+    componentDidMount() {
+        fetch('/data/dealers')
+            .then(response => response.json())
+            .then(data => this.setState({ dealers: data }));
+    }
     render() {
 
-        const { logged } = ibexApp;
+        const { dealers } = this.state;
+        const columns = [{
+            Header: 'ID',
+            accessor: 'dealer_id'
+        },{
+            Header: 'Name',
+            accessor: 'name'
+        },{
+            Header: 'Address',
+            accessor: 'address'
+        },{
+            Header: 'City',
+            accessor: 'city'
+        },{
+            Header: 'State',
+            accessor: 'state'
+        },{
+            Header: 'Zip',
+            accessor: 'zip'
+        }];
 
         return (
             <Fragment>
-                <div className="dealers grid">
-                    grid of dealers
-
-                    <p>API host variable {__API_HOST__}</p>
+                <div className="dealers container-fluid">
+                    <div class="card">
+                        <div className="card-title">Dealers</div>
+                        <ReactTable
+                            data={dealers}
+                            columns={columns}
+                            defaultPageSize = {10}
+                            pageSizeOptions = {[10, 20]}
+                            minRows = {1}
+                            // showPagination = {false}
+                        />
+                    </div>
                 </div>
             </Fragment>
         )
